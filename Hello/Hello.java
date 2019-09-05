e67bf2f43ae0c336d6d3659f482b5bf16cefb482
Trevon Phillips <tphillips@eagles.bridgewater.edu>
2019-09-05 19:56:51 +0000

    fixed Hello.java

--- Hello/Hello.java
+++ Hello/Hello.java
@@ -1,3 +1,5 @@
-public static void main (string args []){
-	System.out.println ("Hello World!");
-	}!
+class Hello {
+	public static void main(String args []){
+		System.out.println("Hello World!");
+	}
+}
