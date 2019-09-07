b54040f691f214d33fa2bb28c7856281e3d37bd1
Trevon Phillips <tphillips@eagles.bridgewater.edu>
2019-09-07 22:51:06 +0000

    done

---
+++ Info/Info.java
@@ -0,0 +1,8 @@
+public class Info {
+	public static void main (String args[]){
+	 System.out.println ("Trevon Phillips");
+	 System.out.println ("Computer Science");
+	 System.out.println ("Freshman");
+	}
+}
+
