3c198c5fa4558b66f645aa7898cef153d9734e7e
Trevon Phillips <tphillips@eagles.bridgewater.edu>
2019-11-15 04:09:09 +0000

    $exit
     enter the commit message for your changes. Lines starting
    done
    :wq
    $exit

---
+++ Kattis/Everywhere/Everywhere.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+class Everywhere{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int[] testCase = sc.nextInt[];
+		for(int i = 0;i<testCase.length;i++){
+			int [i] = sc.nextInt();
+			String [] s = new String [s];
+			cityName[i] = s.nextLine();
+		}
+		System.out.println(testCase);
+	}
+}
+		
